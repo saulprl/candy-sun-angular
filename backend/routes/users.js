ac0297cc429138e58dfd8bfb2b7bd9a93516f194
phasecross <phasecross@hotmail.com>
2020-05-04 22:07:44 -0700

    "Spaced code out"

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,9 +6,13 @@ const userController = require('../controllers/user-controller');
 const router = express.Router();
 
 router.post('/login', userController.login);
+
 router.get('/', checkAuth, userController.getUsers);
+
 router.put('/:_id', checkAuth, userController.updateUser);
+
 router.delete('/:_id', checkAuth, userController.deleteUser);
+
 router.post('/signup', checkAuth, userController.createUser);
 
 module.exports = router;
